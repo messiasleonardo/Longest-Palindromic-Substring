49a1cf7f96e9cbed5e4ef5be19fd7ba089f2e0c0
Leonardo Alves <leonardo.alves@eritel.com.br>
2022-06-21 16:54:25 -0300

    Att, check new methos, needs fixed the result

--- Longest Palindromic Substring/Solution.cs
+++ Longest Palindromic Substring/Solution.cs
@@ -10,99 +10,66 @@ namespace Longest_Palindromic_Substring
     {
         public string LongestPalindrome(string s)
         {
-            //char[] convertString = s.ToArray();
-            //string invertString = "";
-            //for (int i = convertString.Length - 1; i >= 0; i--)
-            //{
-            //    invertString += $"{convertString[i]}";
-            //}
-
-            //string logeststring = "";
-            //int count = 0;
-            //int totalString = 0;
-            //string result = "";
-
-
-
-            //char[] invertStringArray = invertString.ToArray();
-
-            //for (int i = 0; i <= invertString.Length - 1; i++)
-            //{
-            //    for (int x = 0; x <= invertString.Length - 1; x++)
-            //    {
-            //        if (invertStringArray[i] == convertString[x])
-            //        {
-            //            for (int z = i, a = x; z <= invertString.Length - 1; z++, a++)
-            //            {
-            //                if (z > invertString.Length - 1 || a > invertString.Length - 1)
-            //                {
-            //                    break;
-            //                }
-            //                else
-            //                {
-            //                    if (invertStringArray[z] == convertString[a])
-            //                    {
-            //                        count++;
-            //                        logeststring += $"{invertStringArray[z]}";
-            //                    }
-            //                    else
-            //                    {
-            //                        count = 0;
-            //                        logeststring = "";
-            //                    }
-            //                    if (count > totalString)
-            //                    {
-            //                        result = logeststring;
-            //                        totalString = count;
-
-            //                    }
-            //                }
-            //            }
-            //            logeststring = "";
-            //            count = 0;
-            //        }
-            //    }
-
-            //}
-
-            //return result;
-
-
-            int count = findAllPalindromeSubstrings(s);
-            return count.ToString();
-        }
-
-        private int findAllPalindromeSubstrings(string s)
-        {
-            string test = "";
-            int count = 0;
-            char[] inputs = s.ToArray();
-            for (int i = 0; i < s.Length; ++i)
+            char[] convertString = s.ToArray();
+            string invertString = "";
+            for (int i = convertString.Length - 1; i >= 0; i--)
             {
-                count += findPalindromesInSubString(s, i - 1, i + 1);
-                count += findPalindromesInSubString(s, i, i + 1);
-                test += $"{inputs[i]}";
+                invertString += $"{convertString[i]}";
             }
 
-            return count;
-        }
-
-        private int findPalindromesInSubString(string s, int j, int k)
-        {
-            char[] inputs = s.ToArray();
+            string logeststring = "";
             int count = 0;
-            
-            for (; j >= 0 && k < s.Length; --j, ++k)
+            int totalString = 0;
+            string result = "";
+
+
+
+            char[] invertStringArray = invertString.ToArray();
+
+            for (int i = 0; i <= invertString.Length - 1; i++)
             {
-                if (inputs[j] != inputs[k])
+                for (int x = 0; x <= invertString.Length - 1; x++)
                 {
-                    break;
+                    if (invertStringArray[i] == convertString[x])
+                    {
+                        for (int z = i, a = x; z <= invertString.Length - 1; z++, a++)
+                        {
+                            if (z > invertString.Length - 1 || a > invertString.Length - 1)
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                if (invertStringArray[z] == convertString[a])
+                                {
+                                    count++;
+                                    logeststring += $"{invertStringArray[z]}";
+                                }
+                                else
+                                {
+                                    count = 0;
+                                    logeststring = "";
+                                }
+                                if (count > totalString)
+                                {
+                                    result = logeststring;
+                                    totalString = count;
+
+                                }
+                            }
+                        }
+                        logeststring = "";
+                        count = 0;
+                    }
                 }
 
-                count++;
             }
 
-            return count;
+            return result;
+
+
         }
+
+        
     }
 }
